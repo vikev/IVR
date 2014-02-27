79322a396026b0f645e071cacc9404f322da51df
vikev <lubomirvikev@gmail.com>
2014-02-27 21:52:52 +0000

    unsecessful attempt for background subtraction using Gaussian average

--- drawPerim.m
+++ drawPerim.m
@@ -1,11 +1,11 @@
 function [image, perim] = drawPerim(originalImage, binaryImage, oldPerim)
      perim = bwperim(binaryImage);
      [m,n,rS]=size(perim);
-     for i=1 : m
-     for j =1 : n
-        perim(i,j)=max(perim(i,j)-oldPerim(i,j),0);
-     end
-     end
+     %for i=1 : m
+     %for j =1 : n
+     %   perim(i,j)=max(perim(i,j)-oldPerim(i,j),0);
+     %end
+     %end
      
      
      image=originalImage;
