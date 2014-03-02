a68de6ce2f89df1ea20e0d3b3403c8942e2e4652
Dirzys <l.dirzys@yahoo.com>
2014-03-02 13:31:10 +0000

    mine

--- bwBall.m
+++ bwBall.m
@@ -20,7 +20,6 @@ path = [];
 
 % Read one frame at a time.
 for k = 2 : size(filenames, 1)
-    disp(k);
     frame = imread([file_dir filenames(k).name]);
     gray=rgb2gray(frame);
     Bim = zeros(x,y);
@@ -49,7 +48,7 @@ for k = 2 : size(filenames, 1)
     if k>learnSize
         for i=1:x,
             for j=1:y
-                if gray(i,j)<Min(i,j)-5 || gray(i,j)>Max(i,j)+5
+                if gray(i,j) < Min(i,j)-5 || gray(i,j)>Max(i,j)+5
                     Bim(i,j)=1;
                 end
             end
@@ -63,6 +62,9 @@ for k = 2 : size(filenames, 1)
     
     % Show frame
     imshow(frame);
+    %subplot(1, 2, 1, 'align'), imshow(Bim, 'InitialMagnification', 100, 'Border','tight');
+    %subplot(1, 2, 2, 'align'), imshow(frame, 'InitialMagnification', 100, 'Border','tight');
+    %truesize
     
     % Draw object's centres on the frame
     centres = drawCentres(Bim);
@@ -71,16 +73,22 @@ for k = 2 : size(filenames, 1)
     % TODO only works if one object on the frame
     if ~isempty(centres)
         path = [path ; [centres(1, 1) centres(1,2)]];
+        %if length(path) > 3
+        %    dis = distance(path(end-1, 1), centres(1,1), path(end-1, 2), centres(1,2));
+        %    if dis > 10
+        %        dis
+        %    end
+        %end
     end
     
     % Draw path of the object on the frame
-    %drawPath(path);
+    drawPath(path);
     
     % Draw highest point if the object is on it's highest point
-    [p1, p2] = size(path);
-    if p1 > 2
-        drawHighest(path, Bim);
-    end
+    %[p1, p2] = size(path);
+    %if p1 > 2
+    %    drawHighest(path, Bim);
+    %end
     
     %h2 = rectangle('position',[ 150 40 80 70]);
     %set(h2,'EdgeColor','w','LineWidth',2)
