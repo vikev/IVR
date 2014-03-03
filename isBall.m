a27d45fe8db67efd10e112e13c426d99d3b3342c
vikev <lubomirvikev@gmail.com>
2014-03-03 23:46:24 +0000

    Revert "removed stupid print statement"
    This reverts commit 4d631f5ae51ecbd5aea9ef6d3b7733dace5a63fe.

--- isBall.m
+++ isBall.m
@@ -3,6 +3,7 @@ is = false;
 if P>20 && A>100
     comp = 2 * sqrt(A*pi)/P;
     roundness =  4*pi*A/P^2;
+    disp('vals:');
     if (comp > 0.90 && roundness >0.80) || (comp > 0.80 && roundness >0.90) 
         is = true;
     end    
