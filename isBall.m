e55f21b4aff141915a5abba39713526d1815bc82
vikev <lubomirvikev@gmail.com>
2014-03-03 17:06:34 +0000

    new background subtraction and background adaptation

--- isBall.m
+++ isBall.m
@@ -2,8 +2,7 @@ function is = isBall(P, A)
 is = false;
 if P>20 && A>100
     comp = 2 * sqrt(A*pi)/P;
-    roundness =  4*pi*A/P^2;
-    if (comp > 0.90 && roundness >0.80) || (comp > 0.80 && roundness >0.90) 
+    if comp > 0.80 
         is = true;
     end    
 end
