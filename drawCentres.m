26a2b3b35136c75547ece52d68b22cc447703bb9
vikev <lubomirvikev@gmail.com>
2014-03-04 11:00:32 +0000

    cleanup, some tweaks, minArea parameter added

--- drawCentres.m
+++ drawCentres.m
@@ -1,7 +1,7 @@
 function drawCentres(centre)
     % Draw centres of the objects
     hold on;
-    plot(int32(centre(1)),int32(centre(2)),'o');
+    plot(int32(centre(1)),int32(centre(2)),'.','Color',[1 0 0]);
     hold off;
      
      
--- example/updateUnassignedTracks.m
+++
@@ -1,8 +0,0 @@
- function updateUnassignedTracks
-        for i = 1:length(unassignedTracks)
-            ind = unassignedTracks(i);
-            tracks(ind).age = tracks(ind).age + 1;
-            tracks(ind).consecutiveInvisibleCount = ...
-                tracks(ind).consecutiveInvisibleCount + 1;
-        end
-    end
